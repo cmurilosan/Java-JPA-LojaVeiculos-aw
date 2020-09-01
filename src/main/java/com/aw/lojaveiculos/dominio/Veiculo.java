7c1a1acd5ab11a011a758043b6cfbba6ac1be9a6
Cassio Murilo <cmurilo.san@gmail.com>
2020-08-31 22:22:20 -0300

    01B - Implementando o equals() e hashCode()

--- src/main/java/com/aw/lojaveiculos/dominio/Veiculo.java
+++ src/main/java/com/aw/lojaveiculos/dominio/Veiculo.java
@@ -45,6 +45,31 @@ public class Veiculo {
 	}
 	public void setValor(BigDecimal valor) {
 		this.valor = valor;
-	}	
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Veiculo other = (Veiculo) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
+	
 
 }
